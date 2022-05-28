8618470ca42f203ed7cb872b0e43969473f604b4
hasancali <72003584+hasancali@users.noreply.github.com>
2022-05-28 17:52:38 +0300

    Create

---
+++ component---src-pages-index-js-2fec44a161cf5638d0d3.js
@@ -0,0 +1,2 @@
+(self.webpackChunkv4=self.webpackChunkv4||[]).push([[678],{7704:function(e,t,n){"use strict";n.r(t);var a=n(7294),l=n(9),c=n(7140),r=l.ZP.main.withConfig({displayName:"pages__StyledMainContainer",componentId:"sc-1tzx2o8-0"})(["counter-reset:section;"]);t.default=function(e){var t=e.location;return a.createElement(c.Ar,{location:t},a.createElement(r,{className:"fillHeight"},a.createElement(c.VM,null),a.createElement(c.r8,null)))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-2fec44a161cf5638d0d3.js.map
